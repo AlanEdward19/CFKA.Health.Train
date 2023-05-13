ad99efcded1d102a2dd76ec64da94f52571e3a5f
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:20:50 -0300

    feat: endpoint configuration

---
+++ src/CFKA.Health.Train.API/Configurations/Endpoint.cs
@@ -0,0 +1,19 @@
+﻿namespace CFKA.Health.Configurations;
+
+public static class Endpoint
+{
+    public static IApplicationBuilder ConfigureEndpoints(this IApplicationBuilder app, IConfigurationSection section)
+    {
+        var apiHealthCheckUrl = section["APIHealthCheckUrl"];
+
+        app
+            .UseRouting()
+            .UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapHealthChecks(apiHealthCheckUrl);
+        });
+
+        return app;
+    }
+}

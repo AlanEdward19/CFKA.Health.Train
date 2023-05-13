2f09d6f928c1d3134e8b3e1df03c5509786df3a6
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:24:10 -0300

    feat: command for exercises

---
+++ src/CFKA.Health.Train.Application/Commands/CreateUpdateExercise/CreateUpdateExerciseCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace CFKA.Health.Application.Commands.CreateUpdateExercise;
+
+public class CreateUpdateExerciseCommand : ICommand
+{
+    public string Name { get; set; }
+    public string Muscle { get; set; }
+}
---
+++ src/CFKA.Health.Train.Application/Commands/CreateUpdateExercise/CreateUpdateExerciseCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using CFKA.Health.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CFKA.Health.Application.Commands.CreateUpdateExercise;
+
+public class CreateUpdateExerciseCommandHandler : ICommandHandler
+{
+    private readonly IRepository<Exercise> _repository;
+    private readonly DbSet<Muscle> _muscles;
+
+    public CreateUpdateExerciseCommandHandler(IRepository<Exercise> repository, CFKATrainDbContext dbContext)
+    {
+        _repository = repository;
+        _muscles = dbContext.Muscles;
+    }
+
+    public async Task Insert(ICommand command)
+    {
+        var model = command as CreateUpdateExerciseCommand;
+        Muscle muscle= await _muscles.Where(x => x.Name.ToLower().Equals(model!.Muscle.ToLower())).FirstAsync();
+
+        Exercise exercise = new()
+        {
+            Name = model.Name,
+            Muscle = muscle
+        };
+
+        await _repository.AddAsync(exercise);
+    }
+
+    public async Task Update(ICommand command, int id)
+    {
+        var model = command as CreateUpdateExerciseCommand;
+        Muscle muscle = await _muscles.Where(x => x.Name.ToLower().Equals(model!.Muscle.ToLower())).FirstAsync();
+
+        Exercise exercise = new()
+        {
+            Name = model.Name,
+            Muscle = muscle
+        };
+
+        await _repository.UpdateAsync(exercise, id);
+    }
+
+    public async Task Delete(int id) => await _repository.DeleteById(id);
+}

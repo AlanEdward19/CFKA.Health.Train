691a9a9b076a0e8a426ed3ddff9889a20767a543
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:25:17 -0300

    feat: muscle and exercise entity

---
+++ src/CFKA.Health.Train.Domain/Entities/Exercise.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CFKA.Health.Domain.Entities;
+
+public class Exercise
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public Muscle Muscle { get; set; }
+}
---
+++ src/CFKA.Health.Train.Domain/Entities/Muscle.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CFKA.Health.Domain.Entities;
+
+public class Muscle
+{
+    [Key]
+    public int Id { get; set; }
+    public EMuscle MainMuscle { get; set; }
+    public string Name { get; set; }
+}

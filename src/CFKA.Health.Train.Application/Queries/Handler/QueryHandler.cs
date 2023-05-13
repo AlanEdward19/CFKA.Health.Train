ae5546629094058a3b635efff92d432f57d31831
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:25:05 -0300

    feat: generic query handler

---
+++ src/CFKA.Health.Train.Application/Queries/Handler/QueryHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace CFKA.Health.Application.Queries.Handler;
+
+public class QueryHandler<T>
+{
+    private readonly IQueryHandler<T> _handler;
+
+    public QueryHandler(IQueryHandler<T> handler)
+    {
+        _handler = handler;
+    }
+
+    public async Task<T> Handle(int id) =>  await _handler.GetById(id);
+    public async Task<List<T>> Handle() => await _handler.GetAll();
+}

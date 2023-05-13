5b5f832bb241501f9ad863c9c51ee08e0dda254d
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:24:20 -0300

    feat: command for muscles

---
+++ src/CFKA.Health.Train.Application/Commands/CreateUpdateMuscle/CreateUpdateMuscleCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace CFKA.Health.Application.Commands.CreateUpdateMuscle;
+
+public class CreateUpdateMuscleCommand : ICommand
+{
+    [EnumValidator(EnumType = typeof(EMuscle))]
+    public string MainMuscle { get; set; }
+    public string Name { get; set; }
+}
---
+++ src/CFKA.Health.Train.Application/Commands/CreateUpdateMuscle/CreateUpdateMuscleCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿namespace CFKA.Health.Application.Commands.CreateUpdateMuscle;
+
+public class CreateUpdateMuscleCommandHandler : ICommandHandler
+{
+    private readonly IRepository<Muscle> _repository;
+
+    public CreateUpdateMuscleCommandHandler(IRepository<Muscle> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Insert(ICommand command)
+    {
+        var model = command as CreateUpdateMuscleCommand;
+        EMuscle mainMuscle = Enum.Parse<EMuscle>(model!.MainMuscle, true);
+
+        Muscle muscle = new()
+        {
+            Name = model.Name,
+            MainMuscle = mainMuscle
+        };
+
+        await _repository.AddAsync(muscle);
+    }
+
+    public async Task Update(ICommand command, int id)
+    {
+        var model = command as CreateUpdateMuscleCommand;
+        EMuscle mainMuscle = Enum.Parse<EMuscle>(model!.MainMuscle, true);
+
+        Muscle muscle = new()
+        {
+            Name = model.Name,
+            MainMuscle = mainMuscle
+        };
+
+        await _repository.UpdateAsync(muscle, id);
+    }
+
+    public async Task Delete(int id) => await _repository.DeleteById(id);
+}

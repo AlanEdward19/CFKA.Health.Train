1300c0d797c15e64cbf963461259ceb342ddcbaa
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:25:40 -0300

    feat: interface for comamnd and query handler

---
+++ src/CFKA.Health.Train.Domain/Interfaces/Handler/ICommandHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace CFKA.Health.Domain.Interfaces.Handler;
+
+public interface ICommandHandler
+{
+    Task Insert(ICommand command);
+    Task Update(ICommand command, int id);
+    Task Delete(int id);
+}
---
+++ src/CFKA.Health.Train.Domain/Interfaces/Handler/IQueryHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace CFKA.Health.Domain.Interfaces.Handler;
+
+public interface IQueryHandler<T>
+{
+    Task<List<T>> GetAll();
+    Task<T> GetById(int id);
+}

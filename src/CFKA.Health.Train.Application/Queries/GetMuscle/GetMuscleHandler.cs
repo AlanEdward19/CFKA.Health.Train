53a38dfbe6107188d0a397017aa43b423cc7e6b7
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:24:55 -0300

    feat: query for muscles

---
+++ src/CFKA.Health.Train.Application/Queries/GetMuscle/GetMuscleHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace CFKA.Health.Application.Queries.GetMuscle;
+
+public class GetMuscleHandler : IQueryHandler<Muscle>
+{
+    private readonly IRepository<Muscle> _repository;
+
+    public GetMuscleHandler(IRepository<Muscle> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<Muscle>> GetAll() => await _repository.GetAll();
+    public async Task<Muscle> GetById(int id) => await _repository.GetById(id);
+}

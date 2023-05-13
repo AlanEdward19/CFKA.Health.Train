3521b9453ea569cce98b4e5a2f4c7743943e3c86
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:24:47 -0300

    feat: query for exercises

---
+++ src/CFKA.Health.Train.Application/Queries/GetExercise/GetExerciseHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace CFKA.Health.Application.Queries.GetExercise;
+
+public class GetExerciseHandler : IQueryHandler<Exercise>
+{
+    private readonly IRepository<Exercise> _repository;
+
+    public GetExerciseHandler(IRepository<Exercise> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<List<Exercise>> GetAll() => await _repository.GetAll();
+    public async Task<Exercise> GetById(int id) => await _repository.GetById(id);
+}

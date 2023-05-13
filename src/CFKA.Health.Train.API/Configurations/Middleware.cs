68ac690f923b35c08b8ddc7f4803be2bdb946fb5
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:20:59 -0300

    feat: middleware implemented

---
+++ src/CFKA.Health.Train.API/Configurations/Middleware.cs
@@ -0,0 +1,8 @@
+﻿using CFKA.Health.Middlewares;
+
+namespace CFKA.Health.Configurations;
+
+public static class Middleware
+{
+    public static IApplicationBuilder ConfigureMiddlewares(this IApplicationBuilder app) => app.UseMiddleware<ExceptionMiddleware>();
+}

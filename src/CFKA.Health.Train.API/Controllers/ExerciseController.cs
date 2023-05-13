c87e129441d850cbabdf990d80d6dc9c0c0bda7a
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:22:53 -0300

    feat: controller for CRUD of muscles and exercises

---
+++ src/CFKA.Health.Train.API/Controllers/ExerciseController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CFKA.Health.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ExerciseController : ControllerBase
+    {
+        private readonly CommandHandler _commandHandler;
+        private readonly QueryHandler<Exercise> _queryHandler;
+
+        public ExerciseController(CreateUpdateExerciseCommandHandler commandHandler, GetExerciseHandler queryHandler)
+        {
+            _commandHandler = new(commandHandler);
+            _queryHandler = new(queryHandler);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id) => Ok(await _queryHandler.Handle(id));
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() => Ok(await _queryHandler.Handle());
+
+        [HttpPut]
+        public async Task Update([FromBody] CreateUpdateExerciseCommand command, int id) =>
+            await _commandHandler.Handle(command, id);
+
+        [HttpDelete]
+        public async Task Delete(int id) => await _commandHandler.Handle(id);
+
+        [HttpPost]
+        public async Task Insert([FromBody]CreateUpdateExerciseCommand command) => await _commandHandler.Handle(command);
+    }
+}
---
+++ src/CFKA.Health.Train.API/Controllers/MuscleController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CFKA.Health.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MuscleController : ControllerBase
+    {
+        private readonly CommandHandler _commandHandler;
+        private readonly QueryHandler<Muscle> _queryHandler;
+
+        public MuscleController(CreateUpdateMuscleCommandHandler commandHandler, GetMuscleHandler queryHandler)
+        {
+            _commandHandler = new(commandHandler);
+            _queryHandler = new(queryHandler);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id) => Ok(await _queryHandler.Handle(id));
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() => Ok(await _queryHandler.Handle());
+
+        [HttpPut]
+        public async Task Update([FromBody] CreateUpdateMuscleCommand command, int id) =>
+            await _commandHandler.Handle(command, id);
+
+        [HttpDelete]
+        public async Task Delete(int id) => await _commandHandler.Handle(id);
+
+        [HttpPost]
+        public async Task Insert([FromBody]CreateUpdateMuscleCommand command) => await _commandHandler.Handle(command);
+    }
+}

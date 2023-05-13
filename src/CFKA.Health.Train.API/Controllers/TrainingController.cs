6016b8e5d8ea2e2f689e3a4e9b2b2d898a9a82d3
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:48:12 -0300

    feat: training controller for methods training related

---
+++ src/CFKA.Health.Train.API/Controllers/TrainingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CFKA.Health.Train.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TrainingController : ControllerBase
+    {
+    }
+}

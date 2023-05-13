142eee0497b797ff05305c42cdad3642ef14b37e
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:26:57 -0300

    feat: dbSet extension

---
+++ src/CFKA.Health.Train.Infrasctructure/Extensions/DbSetExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CFKA.Health.Infrastructure.Extensions;
+
+public static class DbSetExtensions
+{
+    public static IQueryable<T> VirtualInclude<T>(this DbSet<T> dbSet) where T : class
+    {
+        var properties = dbSet
+            .GetType()
+            .GetProperties()
+            .Where(p => p.PropertyType.Name.Contains("IEnumerable") && p.PropertyType.IsGenericType)
+            .Select(p => p.Name);
+
+        return properties.Aggregate(dbSet.AsQueryable(), (current, property) => current.Include(property));
+    }
+}

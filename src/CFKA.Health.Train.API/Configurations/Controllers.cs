5d5f6668b262787d43e3374c022cff2fdcab0630
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:20:42 -0300

    feat: controller configurations

---
+++ src/CFKA.Health.Train.API/Configurations/Controllers.cs
@@ -0,0 +1,17 @@
+﻿using CFKA.Health.Filters;
+
+namespace CFKA.Health.Configurations;
+
+public static class Controllers
+{
+    public static IServiceCollection ConfigureController(this IServiceCollection services)
+    {
+        services
+            .AddControllers(options =>
+            {
+                options.Filters.Add<ValidationFilter>();
+            });
+
+        return services;
+    }
+}

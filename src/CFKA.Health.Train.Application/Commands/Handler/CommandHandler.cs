5627cd244ceddee46c8c62b9bf53850d3c10cdb2
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:24:33 -0300

    feat: generic command handler

---
+++ src/CFKA.Health.Train.Application/Commands/Handler/CommandHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace CFKA.Health.Application.Commands.Handler;
+
+public class CommandHandler
+{
+    private readonly ICommandHandler _handler;
+
+    public CommandHandler(ICommandHandler handler)
+    {
+        _handler = handler;
+    }
+
+    public async Task Handle(ICommand command, int id) => await _handler.Update(command, id);
+    public async Task Handle(ICommand command) => await _handler.Insert(command);
+    public async Task Handle(int id) => await _handler.Delete(id);
+}

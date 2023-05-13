3e2475edf030aa510217e38d3b090ae9cec56013
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:26:30 -0300

    feat: dbContext for training related tables

---
+++ src/CFKA.Health.Train.Infrasctructure/Context/CFKATrainDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CFKA.Health.Infrastructure.Context
+{
+    public class CFKATrainDbContext : DbContext
+    {
+        public CFKATrainDbContext(DbContextOptions<CFKATrainDbContext> options) : base(options) { }
+
+        #region Db Sets
+
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<Muscle> Muscles { get; set; }
+
+        #endregion
+    }
+
+}

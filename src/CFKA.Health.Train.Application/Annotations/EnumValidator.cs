7605c6401832beec7ee7c38060273b2be0154f46
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-13 16:23:47 -0300

    feat: annotation for enum validation

---
+++ src/CFKA.Health.Train.Application/Annotations/EnumValidator.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CFKA.Health.Application.Annotations;
+
+public class EnumValidator : ValidationAttribute
+{
+    public Type EnumType { get; set; }
+    public string[]? ForceInvalidValues { get; set; }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is "")
+            return ValidationResult.Success;
+
+        if (value is null)
+        {
+            throw new Exception($"Nenhum parametro foi especificado {validationContext.DisplayName}");
+        }
+        else
+        {
+            if (Enum.TryParse(EnumType, value.ToString(), true, out var result))
+            {
+                if (ForceInvalidValues is not null)
+                {
+                    if (ForceInvalidValues.ToList().Contains(result.ToString(), StringComparer.InvariantCultureIgnoreCase))
+                    {
+                        throw new Exception($"Invalid value: {value.ToString()} for {EnumType.ToString()} {validationContext.DisplayName}");
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception($"Invalid value: {value.ToString()} for {EnumType.ToString()} {validationContext.DisplayName}");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}

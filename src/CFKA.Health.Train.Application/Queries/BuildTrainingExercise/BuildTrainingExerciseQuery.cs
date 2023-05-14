3ee6634eeeb692358df2fd1d455414a02797392b
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-14 19:11:37 -0300

    feat: training related queries

---
+++ src/CFKA.Health.Train.Application/Queries/BuildTrainingExercise/BuildTrainingExerciseQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace CFKA.Health.Train.Application.Queries.BuildTrainingExercise;
+
+public class BuildTrainingExerciseQuery
+{
+    public string Exercise { get; set; }
+    public int Sets { get; set; }
+    public int Reps { get; set; }
+    public IEnumerable<string> Observations { get; set; }
+}

325b74e735c30253f9613c238cf3d55fe50199e5
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-14 19:11:04 -0300

    feat: training related controller

--- src/CFKA.Health.Train.API/Controllers/TrainingController.cs
+++ src/CFKA.Health.Train.API/Controllers/TrainingController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CFKA.Health.Train.Application.Handler;
+using CFKA.Health.Train.Application.Queries.BuildTrainingSheet;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CFKA.Health.Train.API.Controllers
@@ -7,5 +8,17 @@ namespace CFKA.Health.Train.API.Controllers
     [ApiController]
     public class TrainingController : ControllerBase
     {
+        private readonly TrainingHandler _handler;
+
+        public TrainingController(TrainingHandler handler)
+        {
+            _handler = handler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BuildTrainingSheet([FromBody] BuildTrainingSheetQuery query)
+        {
+            return Ok(_handler.BuildTrainingSheet(query));
+        }
     }
 }

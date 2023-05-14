bd62c806d888571884f715d4a3ae78dc0ab65ae1
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-14 19:12:30 -0300

    feat: added base entities on model creating database

--- src/CFKA.Health.Train.Infrasctructure/Context/CFKATrainDbContext.cs
+++ src/CFKA.Health.Train.Infrasctructure/Context/CFKATrainDbContext.cs
@@ -1,10 +1,34 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CFKA.Health.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
 
 namespace CFKA.Health.Infrastructure.Context
 {
     public class CFKATrainDbContext : DbContext
     {
         public CFKATrainDbContext(DbContextOptions<CFKATrainDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Muscle>().HasData(new List<Muscle>()
+            {
+                #region Chest
+
+                new()
+                {
+                    Name = "Upper Chest",
+                    MainMuscle = EMuscle.Chest
+                },
+                new()
+                {
+                    Name = "Lower Chest",
+                    MainMuscle = EMuscle.Chest
+                }
+
+                #endregion
+            });
+        }
 
         #region Db Sets
 

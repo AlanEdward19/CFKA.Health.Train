ae79f0bc45458f29e83f3d96eae9aa6861641837
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-15 11:57:39 -0300

    feat: implemented basic secundary muscles groups

--- src/CFKA.Health.Train.Infrasctructure/Context/CFKATrainDbContext.cs
+++ src/CFKA.Health.Train.Infrasctructure/Context/CFKATrainDbContext.cs
@@ -13,6 +13,8 @@ namespace CFKA.Health.Infrastructure.Context
 
             modelBuilder.Entity<Muscle>().HasData(new List<Muscle>()
             {
+                #region Muscles
+
                 #region Chest
 
                 new()
@@ -22,9 +24,182 @@ namespace CFKA.Health.Infrastructure.Context
                 },
                 new()
                 {
+                    Name = "Middle Chest",
+                    MainMuscle = EMuscle.Chest
+                },
+                new()
+                {
                     Name = "Lower Chest",
                     MainMuscle = EMuscle.Chest
+                },
+
+                #endregion
+
+                #region Back
+
+                new()
+                {
+                    Name = "Trapezius",
+                    MainMuscle = EMuscle.Back
+                },
+                new()
+                {
+                    Name = "Lats",
+                    MainMuscle = EMuscle.Back
+                },
+                new()
+                {
+                    Name = "Low Back",
+                    MainMuscle = EMuscle.Back
+                },
+
+                #endregion
+
+                #region Biceps
+
+                new()
+                {
+                    Name = "Brachialis",
+                    MainMuscle = EMuscle.Biceps
+                },
+                new()
+                {
+                    Name = "Brachioradialis",
+                    MainMuscle = EMuscle.Biceps
+                },
+                new()
+                {
+                    Name = "Biceps Brachii",
+                    MainMuscle = EMuscle.Biceps
+                },
+
+                #endregion
+
+                #region Triceps
+
+                new()
+                {
+                    Name = "Long Head of Triceps",
+                    MainMuscle = EMuscle.Triceps
+                },
+                new()
+                {
+                    Name = "Medial Head of Triceps",
+                    MainMuscle = EMuscle.Triceps
+                },
+                new()
+                {
+                    Name = "Lateral Head of Triceps",
+                    MainMuscle = EMuscle.Triceps
+                },
+
+                #endregion
+
+                #region Abs
+
+                new()
+                {
+                    Name = "Upper Abs",
+                    MainMuscle = EMuscle.Abs
+                },
+                new()
+                {
+                    Name = "Lower Abs",
+                    MainMuscle = EMuscle.Abs
+                },
+                new()
+                {
+                    Name = "Obliques",
+                    MainMuscle = EMuscle.Abs
+                },
+
+                #endregion
+
+                #region Calves
+
+                new()
+                {
+                    Name = "Gastrocnemius (Upper Calf)",
+                    MainMuscle = EMuscle.Calves
+                },
+                new()
+                {
+                    Name = "Soleus (Lower Calf)",
+                    MainMuscle = EMuscle.Calves
+                },
+                new()
+                {
+                    Name = "Tibialis Anterior",
+                    MainMuscle = EMuscle.Calves
+                },
+
+                #endregion
+
+                #region Quadriceps
+
+                new()
+                {
+                    Name = "Vastus Lateralis",
+                    MainMuscle = EMuscle.Quadriceps
+                },
+                new()
+                {
+                    Name = "Vastus Medialis",
+                    MainMuscle = EMuscle.Quadriceps
+                },
+                new()
+                {
+                    Name = "Rectus Femoris",
+                    MainMuscle = EMuscle.Quadriceps
+                },
+
+                #endregion
+
+                #region Glutes
+
+                new()
+                {
+                    Name = "Gluteus Maximus",
+                    MainMuscle = EMuscle.Glutes
+                },
+                new()
+                {
+                    Name = "Gluteus Medius",
+                    MainMuscle = EMuscle.Glutes
+                },
+                new()
+                {
+                    Name = "Gluteus Minimus",
+                    MainMuscle = EMuscle.Glutes
+                },
+
+                #endregion
+
+                #region Forearms
+
+                new()
+                {
+                    Name = "Wrist Flexors",
+                    MainMuscle = EMuscle.Forearms
+                },
+                new()
+                {
+                    Name = "Wrist Extensors",
+                    MainMuscle = EMuscle.Forearms
+                },
+                new()
+                {
+                    Name = "Grip",
+                    MainMuscle = EMuscle.Forearms
                 }
+
+                #endregion
+
+                #endregion
+
+                #region Exercises
+
+                
 
                 #endregion
             });

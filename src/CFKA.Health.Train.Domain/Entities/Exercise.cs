418c60dae32b27e1be1f105bfcf31a3f6f8bd0fa
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-18 16:14:14 -0300

    feat: changed muscle to virtual and added foreignKey

--- src/CFKA.Health.Train.Domain/Entities/Exercise.cs
+++ src/CFKA.Health.Train.Domain/Entities/Exercise.cs
@@ -7,5 +7,7 @@ public class Exercise
     [Key]
     public int Id { get; set; }
     public string Name { get; set; }
-    public Muscle Muscle { get; set; }
+    public int MuscleId { get; set; }
+
+    public virtual Muscle Muscle { get; set; }
 }

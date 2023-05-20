8784332a2289d6687cb7af2bb0e87cd0263c5f7b
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-05-19 21:14:15 -0300

    feat: viewModels

---
+++ src/CFKA.Health.Train.Application/ViewModels/TrainingSheetViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace CFKA.Health.Train.Application.ViewModels;
+
+public class TrainingSheetViewModel
+{
+    public IEnumerable<WorkoutViewModel> Workouts { get; set; }
+    public DateTime ChangeDate { get; set; }
+}

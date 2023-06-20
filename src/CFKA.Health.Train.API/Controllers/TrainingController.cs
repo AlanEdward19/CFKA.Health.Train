a3e8cbdabaec102b272f1f619ba7f7061588963f
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-06-20 14:29:32 -0300

    feat: new training routes

--- src/CFKA.Health.Train.API/Controllers/TrainingController.cs
+++ src/CFKA.Health.Train.API/Controllers/TrainingController.cs
@@ -1,4 +1,6 @@
-﻿using CFKA.Health.Train.Application.Handler;
+﻿using CFKA.Health.Domain.Interfaces.Handler;
+using CFKA.Health.Train.Application.Commands.CreateUpdateTraining;
+using CFKA.Health.Train.Application.Handler;
 using CFKA.Health.Train.Application.Queries.BuildTrainingSheet;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,14 +10,28 @@ namespace CFKA.Health.Train.API.Controllers
     [ApiController]
     public class TrainingController : ControllerBase
     {
+        private readonly CommandHandler _commandHandler;
         private readonly TrainingHandler _handler;
 
-        public TrainingController(TrainingHandler handler)
+        public TrainingController(TrainingHandler handler, CreateUpdateTrainingCommandHandler commandHandler)
         {
             _handler = handler;
+            _commandHandler = new(commandHandler);
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id) => Ok(await _handler.GetById(Request.Headers["api-key"],id));
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() => Ok(await _handler.GetAll(Request.Headers["api-key"]));
+
+        [HttpPut]
+        public async Task Update([FromBody] CreateUpdateTrainingCommand command, int id) => await _commandHandler.Handle(command, id);
+
+        [HttpDelete]
+        public async Task Delete(int id) => await _commandHandler.Handle(id);
+
         [HttpPost]
-        public async Task<IActionResult> BuildTrainingSheet([FromBody] BuildTrainingSheetQuery query) => Ok(await _handler.BuildTrainingSheet(query));
+        public async Task Insert([FromBody] CreateUpdateTrainingCommand command) => await _commandHandler.Handle(command);
     }
 }

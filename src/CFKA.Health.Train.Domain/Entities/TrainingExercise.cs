afdcd370f29f5031ab759d2052ae40f27d14a26b
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-06-20 15:21:21 -0300

    fix: fixed training and trainingExercise database relationship

--- src/CFKA.Health.Train.Domain/Entities/TrainingExercise.cs
+++ src/CFKA.Health.Train.Domain/Entities/TrainingExercise.cs
@@ -6,10 +6,12 @@ public class TrainingExercise
 {
     [Key]
     public int Id { get; private set; }
-    public virtual Exercise Exercise { get; private set; }
     public int Sets { get; private set; }
     public int Reps { get; private set; }
     public string Observations { get; private set; }
+
+    public virtual Training Training { get; private set; }
+    public virtual Exercise Exercise { get; private set; }
 
     public TrainingExercise(int sets, int reps, string observations)
     {

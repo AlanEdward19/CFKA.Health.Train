5e68c434a0c8ce32b2c4cb3616edc37c12e44ab5
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-06-20 14:30:41 -0300

    feat: trainingInput model

---
+++ src/CFKA.Health.Train.Application/InputModels/TrainingExerciseInputModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CFKA.Health.Train.Application.InputModels;
+
+public record TrainingExerciseInputModel
+{
+    public int ExerciseId { get; set; }
+    public int Sets { get; set; }
+    public int Reps { get; set; }
+    public string Observations { get; set; }
+
+    public static TrainingExercise ToEntity(Exercise exercise, TrainingExerciseInputModel trainingExerciseInputModel)
+    {
+        TrainingExercise trainingExercise = new(trainingExerciseInputModel.Sets, trainingExerciseInputModel.Reps,
+            trainingExerciseInputModel.Observations);
+
+        trainingExercise.SetExercise(exercise);
+
+        return trainingExercise;
+    }
+}

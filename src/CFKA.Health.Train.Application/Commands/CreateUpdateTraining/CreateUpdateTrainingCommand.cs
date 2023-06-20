d07d53c58ffad1d291f312cd35f28e644ec8e3f0
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-06-20 14:30:10 -0300

    feat: training command and command handler

---
+++ src/CFKA.Health.Train.Application/Commands/CreateUpdateTraining/CreateUpdateTrainingCommand.cs
@@ -0,0 +1,10 @@
+﻿using CFKA.Health.Train.Application.InputModels;
+
+namespace CFKA.Health.Train.Application.Commands.CreateUpdateTraining;
+
+public class CreateUpdateTrainingCommand : ICommand
+{
+    public string Owner { get; set; }
+    public DateTime ChangeDate { get; set; }
+    public IEnumerable<TrainingExerciseInputModel> TrainingExercises { get; set; }
+}

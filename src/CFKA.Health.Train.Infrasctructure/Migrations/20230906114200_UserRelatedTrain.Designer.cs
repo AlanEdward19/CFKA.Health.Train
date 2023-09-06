a0bb7139a137b9536e47e1a764e1186e270d85ba
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-09-06 09:43:17 -0300

    feat: migrations

--- src/CFKA.Health.Train.Infrasctructure/Migrations/20230906114200_UserRelatedTrain.Designer.cs
+++ src/CFKA.Health.Train.Infrasctructure/Migrations/20230906114200_UserRelatedTrain.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CFKA.Health.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CFKA.Health.Infrastructure.Migrations
 {
     [DbContext(typeof(CFKATrainDbContext))]
-    partial class CFKATrainDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230906114200_UserRelatedTrain")]
+    partial class UserRelatedTrain
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

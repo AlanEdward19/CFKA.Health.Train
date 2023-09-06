95166f318f320dfdde8668675d1326d739c1aa0f
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2023-09-06 09:41:29 -0300

    feat: methods for user on repository

--- src/CFKA.Health.Train.Infrasctructure/Repository/Repository.cs
+++ src/CFKA.Health.Train.Infrasctructure/Repository/Repository.cs
@@ -44,11 +44,28 @@ public class Repository<T> : IRepository<T> where T : class
         await _dbContext.SaveChangesAsync();
     }
 
+    public async Task UpdateAsync(T entity, Guid id)
+    {
+        var entityDb = await GetById(id);
+
+        foreach (var property in _dbSet.Entry(entityDb).Properties)
+        {
+            var propertyName = property.Metadata.Name;
+
+            if (propertyName.ToLower() != "id")
+                property.CurrentValue = _dbSet.Entry(entity).Property(propertyName).CurrentValue;
+        }
+
+        _dbSet.Update(entityDb);
+
+        await _dbContext.SaveChangesAsync();
+    }
+
     #endregion
 
     #region Get
 
-    public async Task<List<T>> GetAll() => await _dbSet.VirtualInclude().ToListAsync();
+    public async Task<List<T>> GetAll() => typeof(T) == typeof(User) ? await _dbSet.ToListAsync() : await _dbSet.VirtualInclude().ToListAsync();
 
     public async Task<T> GetById(int id)
     {
@@ -57,7 +74,17 @@ public class Repository<T> : IRepository<T> where T : class
         var lambdaExpression = Expression.Lambda<Func<T, bool>>(Expression.Equal(property, Expression.Constant(id)), parameter);
 
 
-        return await _dbSet.VirtualInclude().Where(lambdaExpression).FirstOrDefaultAsync();
+        return await _dbSet.VirtualInclude().FirstOrDefaultAsync(lambdaExpression);
+    }
+
+    public async Task<T> GetById(Guid id)
+    {
+        var parameter = Expression.Parameter(typeof(T), "x");
+        var property = Expression.Property(parameter, "Id");
+        var lambdaExpression = Expression.Lambda<Func<T, bool>>(Expression.Equal(property, Expression.Constant(id)), parameter);
+
+
+        return await _dbSet.FirstOrDefaultAsync(lambdaExpression);
     }
 
     #endregion
@@ -73,6 +100,14 @@ public class Repository<T> : IRepository<T> where T : class
         await _dbContext.SaveChangesAsync();
     }
 
-    #endregion
+    public async Task DeleteById(Guid id)
+    {
+        var entityDb = await GetById(id);
 
+        _dbSet.Remove(entityDb);
+
+        await _dbContext.SaveChangesAsync();
+    }
+
+    #endregion
 }

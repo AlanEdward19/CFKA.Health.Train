b1c0a78b0c9b5a1b1168a4fd229b0cde5d826161
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2023-11-16 21:37:28 -0300

    feat: added url

--- src/CFKA.Health.Train.Application/ViewModels/ExerciseViewModel.cs
+++ src/CFKA.Health.Train.Application/ViewModels/ExerciseViewModel.cs
@@ -4,14 +4,16 @@ public record ExerciseViewModel
 {
     public string Name { get; private set; }
     public MuscleViewModel Muscle { get; private set; }
+    public string Url { get; set; }
 
-    public ExerciseViewModel(string name, MuscleViewModel muscle)
+    public ExerciseViewModel(string name, MuscleViewModel muscle, string url)
     {
         Name = name;
         Muscle = muscle;
+        Url = url;
     }
 
     public static ExerciseViewModel ToEntity(Exercise entity, ELanguage language) =>
         new(language == ELanguage.English ? entity.EnName : entity.PtName,
-            MuscleViewModel.ToEntity(entity.Muscle, language));
+            MuscleViewModel.ToEntity(entity.Muscle, language), entity.ExerciseVideoUrl);
 }
